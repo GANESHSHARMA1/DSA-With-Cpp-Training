6a41297f0cff5edbeb7efb1fa75f0bc51ccf76c1
GANESHSHARMA1 <ganeshkithana@gmail.com>
2024-05-04 16:20:55 +0530

    Day 13 Code -updated

--- DynamicProgramming.cpp
+++ DynamicProgramming.cpp
@@ -52,16 +52,32 @@ int main()
     // int ans = fib(n, dp);
     // cout << ans;
 
-    // Tabulation(T -> O(n), S -> O(1)) -> Step - 1
-    vector<int> dp(n + 1);
-    dp[0] = 0;
-    dp[1] = 1;
+    //
 
+    // Tabulation(T -> O(n), S -> O(1)) -> Step - 1
+    // vector<int> dp(n + 1);
+    // dp[0] = 0;
+    // dp[1] = 1;
+
+    // for (int i = 2; i <= n; i++)
+    // {
+    //     dp[i] = dp[i - 1] + dp[i - 2];
+    // }
+    // cout << dp[n] << endl;
+
+    //
+
+    // Space Optimization(T -> O(1), S -> O(1)) -> step - 1
+    int prev1 = 1;
+    int prev2 = 0;
+
+    // step 2
     for (int i = 2; i <= n; i++)
     {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
     }
-
-    cout << dp[n] << endl;
+    cout << prev1 << endl;
     return 0;
 }
